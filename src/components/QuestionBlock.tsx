a24dcefccb8830da52ed0054b60b362c17012103
Praveen <spraveenitpro@gmail.com>
2023-10-03 16:47:28 -0700

    Daily update

--- src/components/QuestionBlock.tsx
+++ src/components/QuestionBlock.tsx
@@ -1,7 +1,66 @@
-import React from 'react'
+/* import React from 'react'
+import { Question } from '../../interfaces.ts'
 
-const QuestionBlock = () => {
-    return <div>QuestionBlock</div>
+const QuestionBlock = ({ question }: { question: Question }) => {
+    return (
+        <button
+            className="question-block"
+            //onClick={handleClick}
+        >
+            <img src={question.image} alt={question.alt} />
+            <h3>{question.text}</h3>
+            <p>
+                <a href={question.image}>{question.credit} </a>
+                <a href="https://www.unsplash.com">Unsplash</a>
+            </p>
+        </button>
+    )
+}
+
+export default QuestionBlock
+ */
+
+import React from 'react'
+import { Question } from '../../interfaces'
+
+const QuestionBlock = ({
+    quizItemId,
+    question,
+    chosenAnswerItems,
+    setChosenAnswerItems,
+    unansweredQuestionIds,
+    setUnansweredQuestionIds,
+}: {
+    quizItemId: number
+    question: Question
+    chosenAnswerItems: string[]
+    setChosenAnswerItems: Function
+    unansweredQuestionIds: number[] | undefined
+    setUnansweredQuestionIds: Function
+}) => {
+    /*   const handleClick = () => {
+        setChosenAnswerItems((prevState: string[]) => [...prevState, question.text])
+        setUnansweredQuestionIds(unansweredQuestionIds?.filter((id: number) => id !== quizItemId))
+    }
+
+    const validPick = !chosenAnswerItems?.includes(question.text) &&
+        !unansweredQuestionIds?.includes(quizItemId)
+*/
+
+    return (
+        <button
+            className="question-block"
+            //     onClick={handleClick}
+            //     disabled={validPick}
+        >
+            <img src={question.image} alt={question.alt} />
+            <h3>{question.text}</h3>
+            <p>
+                <a href={question.image}>{question.credit} </a>
+                <a href="https://www.unsplash.com">Unsplash</a>
+            </p>
+        </button>
+    )
 }
 
 export default QuestionBlock
--- src/components/QuestionsBlock.tsx
+++ src/components/QuestionsBlock.tsx
@@ -1,13 +1,14 @@
-import React from 'react'
+/* import React from 'react'
 import { Content, Question } from '../../interfaces.ts'
 import QuestionBlock from './QuestionBlock.tsx'
-
 
 const QuestionsBlock = ({ quizItem }: { quizItem: Content }) => {
     return (
         <>
-            <h2 id={String(quizItem.id)}>{quizItem.text}</h2>
-            <div className="questions-container">
+            <h2 className="title-block" id={String(quizItem.id)}>
+                {quizItem.text}
+            </h2>
+            <div className="questions-container	">
                 {quizItem?.questions.map(
                     (question: Question, _index: number) => (
                         <QuestionBlock key={_index} question={question} />
@@ -19,3 +20,50 @@ const QuestionsBlock = ({ quizItem }: { quizItem: Content }) => {
 }
 
 export default QuestionsBlock
+ */
+
+import React, { forwardRef } from 'react'
+import { Content, Question } from '../../interfaces'
+import QuestionBlock from './QuestionBlock'
+
+const QuestionsBlock = (
+    {
+        quizItem,
+        chosenAnswerItems,
+        setChosenAnswerItems,
+        unansweredQuestionIds,
+        setUnansweredQuestionIds,
+    }: {
+        quizItem: Content
+        chosenAnswerItems: string[]
+        setChosenAnswerItems: Function
+        unansweredQuestionIds: number[] | undefined
+        setUnansweredQuestionIds: Function
+    },
+    ref: React.LegacyRef<HTMLHeadingElement> | undefined
+) => {
+    return (
+        <>
+            <h2 ref={ref} className="title-block">
+                {quizItem.text}
+            </h2>
+            <div className="questions-container">
+                {quizItem?.questions.map(
+                    (question: Question, _index: number) => (
+                        <QuestionBlock
+                            key={_index}
+                            quizItemId={quizItem.id}
+                            question={question}
+                            chosenAnswerItems={chosenAnswerItems}
+                            setChosenAnswerItems={setChosenAnswerItems}
+                            unansweredQuestionIds={unansweredQuestionIds}
+                            setUnansweredQuestionIds={setUnansweredQuestionIds}
+                        />
+                    )
+                )}
+            </div>
+        </>
+    )
+}
+
+export default forwardRef(QuestionsBlock)
