9985f897d4df7f7c2197349ef0e36084f479ff63
Praveen <spraveenitpro@gmail.com>
2023-10-02 23:03:45 -0700

    Daily update

--- src/components/QuestionsBlock.tsx
+++ src/components/QuestionsBlock.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { Content, Question } from '../../interfaces.ts'
+import QuestionBlock from './QuestionBlock.tsx'
 
-const QuestionsBlock = () => {
-  return (
-    <div>QuestionsBlock</div>
-  )
+const QuestionsBlock = ({ quizItem }: { quizItem: Content }) => {
+    return (
+        <>
+            <h2 id={String(quizItem.id)}>{quizItem.text}</h2>
+            <div className="questions-container">
+                {quizItem?.questions.map(
+                    (question: Question, _index: number) => (
+                        <QuestionBlock key={_index} question={question} />
+                    )
+                )}
+            </div>
+        </>
+    )
 }
 
 export default QuestionsBlock
